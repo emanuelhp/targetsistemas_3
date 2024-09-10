e109346a31b580f7729b8ff6ffb06a6c6d16379d
Emanuel Prado <quem1000@gmail.com>
2024-09-10 13:47:58 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿var faturamentoAnual = Enumerable.Range(0, 366)
+    .Select(i => Random.Shared.Next(0, 100000) / 100d)
+    .ToArray();
+
+var faturamentoComValores = faturamentoAnual.Where(i => i > 0).ToArray();
+
+var menorValorFaturamento = faturamentoComValores.Min(); 
+var maiorValorFaturamento = faturamentoComValores.Max();
+var mediaValorFaturamento = faturamentoComValores.Average();
+
+var numeroDiasComFaturamentoMaiorQueAMedia = faturamentoComValores.Count(i => i > mediaValorFaturamento);
+
+Console.WriteLine("Menor faturamento do ano: {0:F2}", menorValorFaturamento);
+Console.WriteLine("Maior faturamento do ano: {0:F2}", maiorValorFaturamento);
+Console.WriteLine("Dias com faturamento acima da média: {0}", numeroDiasComFaturamentoMaiorQueAMedia);
